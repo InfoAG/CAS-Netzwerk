44bc9d9402bbe268df296b3a79b2f30aea7e9f24
Inforaum <paulinumcluster@googlemail.com>
2012-03-30 15:53:34 +0200

    Matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,11 +1,35 @@
-/*
- *  Matrix.cpp
- *  CASMatrix
- *
- *  Created by InfoAG on 30.03.12.
- *  Copyright 2012 __MyCompanyName__. All rights reserved.
- *
- */
-
 #include "Matrix.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int** k = new int*[3];
+	
+	for(int i = 0; i < 3; i++)
+	{
+		k[i] = new int[3];
+	}
+	
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			k[i][j] = rand() % 10+1;
+		}
+	}
+	
+	
+	int* e = new int[3];
+	for(int i = 0; i < 3; i++)
+	{
+		e[i] = rand() % 10+1;
+	}
+	
+	Matrix test(3, k, e);
+	
+	
+	return 0;
+}
 
